3647f4a694b925e30f4bb21d501b4b9295caf60b
JSimjee <jameelsimjee@gmail.com>
2026-04-06 16:17:15 -0700

    Made some changes, still not fully working

--- lab-common/src/adder.sv
+++ lab-common/src/adder.sv
@@ -1,12 +1,13 @@
-module top (
+module adder (
   /** Input Ports */
   input wire [3:0] val1,
   input wire [3:0] val2,
   /** Output Ports */
-  output logic [4:0] res
+  output logic [4:0] res // Five bits in case there's overflow (16-29), turn on decimal
 );
 
 /** Logic */
-
+// Outside of an always block to repeatedly run, 
+assign res = val1 + val2; // Thanks Hooman for telling me I can just use addition, instead of bit-wise addition and manually handling the carry bits and so on
 
 endmodule
--- lab-common/src/top.sv
+++ lab-common/src/top.sv
@@ -3,12 +3,32 @@
 
 module top (
     /** Input Ports */
-        input wire [3:0] val1,
+    input wire [3:0] val1,
     input wire [3:0] val2,
     /** Output Ports */
-    output logic [6:0] seg7
+    output logic [7:0] seg7 // To account for overflow
 );
 
+/*
+Notes
+Create a wire, assign that wire with your desired boolean logic, and assign that to your output logic
+*/
+
 /** Logic */
+// Call the decoder and full adder
+// Input val1/2 into the adder
+// Take the adder's output, make that as input for the decoder
+// Take the decoder's output, make that output for 'top'
+// Remember to map bits correctly (e.g. seg7 in 'decoder' maps to seg7[6:0] in 'top'
+//                                      , and res[4] in 'adder' maps to seg7[7] in 'top')
+
+
+input wire result[4:0];
+output logic new_seg7[7:0];
+always_comb begin
+    // Instantiate adder
+    adder(val1, val2, result);
+    decoder(result[3:0], new_seg7);
+end
 
 endmodule
